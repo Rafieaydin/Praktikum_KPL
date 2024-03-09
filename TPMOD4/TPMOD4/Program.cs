6163578b393a76b8d8f44a11f311009be2c036ea
Rafieaydin <rafie.aydin1@gmail.com>
2024-03-09 20:00:37 +0700

    TPMOD04 init

---
+++ TPMOD4/TPMOD4/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TPMOD4.kodepos;
+using System;
+
+namespace TPMOD4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KodePos kodepos = new KodePos();
+            kodepos.setKodePost();
+            kodepos.getKodePos();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
